2663e6e2a4db46fcd929df4bfb70bf070513f2cc
jonahhess <jonahhessdev@gmail.com>
2025-09-03 23:31:21 +0300

    added validateId, routes send result of controller fns

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -3,40 +3,47 @@ const router = express.Router();
 const {
   validatePost,
   validatePut,
+  validateId,
 } = require("../middlewares/recipeValidator.js");
 const {
   getAllRecipes,
   getStats,
+  getRecipeById,
+  addRecipe,
+  updateRecipeById,
+  deleteRecipeById,
 } = require("../controllers/recipeController.js");
 
 // static routes
 router.get("/", (req, res) => {
   const allRecipes = getAllRecipes();
-  console.log(allRecipes);
-  res.send("getting all recipes");
+  res.send(allRecipes);
 });
 
 router.get("/stats", (req, res) => {
   const stats = getStats();
-  console.log(stats);
-  res.send("getting stats");
+  res.send(stats);
 });
 
 router.post("/", validatePost, (req, res) => {
-  res.send("posting valid recipe");
+  const post = addRecipe(req.body);
+  res.send(post);
 });
 
 // dynamic routes
-router.get("/:id", (req, res) => {
-  res.send("getting recipe by id");
+router.get("/:id", validateId, (req, res) => {
+  const storedRecipe = getRecipeById(req.params.id);
+  res.send(storedRecipe);
 });
 
-router.put("/:id", validatePut, (req, res) => {
-  res.send("putting valid recipe");
+router.delete("/:id", validateId, (req, res) => {
+  const deletion = deleteRecipeById(req.params.id);
+  res.send(deletion);
 });
 
-router.delete("/:id", (req, res) => {
-  res.send("deleting a recipe by id");
+router.put("/:id", validateId, validatePut, (req, res) => {
+  const update = updateRecipeById(req.params.id, req.body);
+  res.send(update);
 });
 
 module.exports = router;
