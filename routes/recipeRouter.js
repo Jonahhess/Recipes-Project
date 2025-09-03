cc4a3321b40881c8494d6f4f2a1a5b266ce4cc23
jonahhess <jonahhessdev@gmail.com>
2025-09-03 17:19:11 +0300

    router sends response

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -1,20 +1,28 @@
 const express = require("express");
-const router = express();
+const router = express.Router();
 const {
-  validatPost,
-  validatPut,
+  validatePost,
+  validatePut,
 } = require("../middlewares/recipeValidator.js");
 
 router.get("/", (req, res) => {
   res.send("getting all recipes");
 });
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  res.send("getting recipe by id");
+});
 
-router.post("/", validatPost, (req, res) => {});
+router.post("/", validatePost, (req, res) => {
+  res.send("posting valid recipe");
+});
 
-router.put("/:id", validatPut, (req, res) => {});
+router.put("/:id", validatePut, (req, res) => {
+  res.send("putting valid recipe");
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  res.send("deleting a recipe by id");
+});
 
 router.get("/stats", (req, res) => {
   res.send("getting stats");
