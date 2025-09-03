03abbfc93e67149b4d6b6894c44df70530279321
jonahhess <jonahhessdev@gmail.com>
2025-09-03 23:59:37 +0300

    added proxy fns to push  changes to json

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,6 +1,34 @@
-const { readFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 const path = require("path");
 const filePath = path.resolve(__dirname, "../data/recipes.json");
 const recipes = JSON.parse(readFileSync(filePath));
 
-module.exports = recipes;
+const getRecipes = () => {
+  return recipes;
+};
+
+const getRecipeByIndex = (index) => {
+  return recipes[index];
+};
+
+const addRecipe = (recipe) => {
+  recipes.push(recipe);
+  writeFileSync(filePath, JSON.stringify(recipes));
+};
+
+const updateRecipe = (index, modifiedRecipe) => {
+  recipes[index] = modifiedRecipe;
+  writeFileSync(filePath, JSON.stringify(recipes));
+};
+
+const deleteRecipe = (index) => {
+  recipes.splice(index, 1);
+  writeFileSync(filePath, JSON.stringify(recipes));
+};
+module.exports = {
+  getRecipes,
+  getRecipeByIndex,
+  addRecipe,
+  updateRecipe,
+  deleteRecipe,
+};
