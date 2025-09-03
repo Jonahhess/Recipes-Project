f8af0c8d551f705c20ab338e603711e6005d7191
jonahhess <jonahhessdev@gmail.com>
2025-09-03 17:55:57 +0300

    static routes before dynamic routes

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -5,15 +5,22 @@ const {
   validatePut,
 } = require("../middlewares/recipeValidator.js");
 
+// static routes
 router.get("/", (req, res) => {
   res.send("getting all recipes");
 });
-router.get("/:id", (req, res) => {
-  res.send("getting recipe by id");
+
+router.get("/stats", (req, res) => {
+  res.send("getting stats");
 });
 
 router.post("/", validatePost, (req, res) => {
   res.send("posting valid recipe");
+});
+
+// dynamic routes
+router.get("/:id", (req, res) => {
+  res.send("getting recipe by id");
 });
 
 router.put("/:id", validatePut, (req, res) => {
@@ -24,7 +31,4 @@ router.delete("/:id", (req, res) => {
   res.send("deleting a recipe by id");
 });
 
-router.get("/stats", (req, res) => {
-  res.send("getting stats");
-});
 module.exports = router;
